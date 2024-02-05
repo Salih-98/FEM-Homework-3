b32cb7a1b99cdfad7a50712fc3363bd96d6f9049
User <salih.sahbegovic1998@gmail.com>
2024-02-05 22:00:09 +0100

    full calculation

--- Homework3.m
+++ Homework3.m
@@ -132,7 +132,6 @@ Kspring_1 = N' * ks * N;
 %% Element 2 - Bernoulli beam
 L2 = sqrt(a^2 + d^2);
 K2 = BernoulliElementStiffnessMatrix(EA_24,EI_24,L2);
-
 alfa12 = atand(a/d);
 alfa22 = 270-alfa12;
 C12 = cosd(alfa12);
@@ -140,10 +139,10 @@ C22 = cosd(alfa22);
 TM2 = getTransformationMatrix(C12,C22);
 
 K2 = TM2' * K2 *TM2;
-
+K2 = applyBC(K2,1,1,1,0,1,1);
 %% Element 3 - truss
 L3 = a + b + c;
-K3_local = EA_t/L3 * [1 -1; -1 1];
+K3_local = EA_t/(L3*2) * [1 -1; -1 1];
 
 % Directional cosines 
 x13 = 0; z13 = 0;
@@ -154,91 +153,8 @@ C23 = (z14 - z13)/L3;
 % Transformation matrix
 TM3 = [C13 C23 0 0; 0 0 C13 C23];
 K3_global = TM3' * K3_local * TM3;
-K3_global_reduced = K3_global(1:2,1:2);
+K3_global_reduced = K3_global(2,2);
 
-%% Element 4 - beam
-L4 = sqrt(a^2 + d^2);
-K4 = BernoulliElementStiffnessMatrix(EA_24,EI_24,L2);
-
-alfa14 = -atand(a/d);
-alfa24 = 270-alfa14;
-C14 = cosd(alfa14);
-C24 = cosd(alfa24);
-TM4 = getTransformationMatrix(C14,C24);
-
-K4red = TM4' * K4 *TM4;
-
-%% Element 5 - beam
-L5 = b;
-
-% Change of rectangle side as a function of ksi
-syms ksi
-side = t2 + ((t1-t2)/L5)*L5*(1+ksi)/2;
-A = expand(side^2);
-A = vpa(A,4);
-
-% B matrix for u 
-Bu = [-1/L5 1/L5];
-
-% Jacobian 
-J = L5/2;
-
-% Element matrix for element 1 - axial part
-K5_loc_axial = Bu' * Eb * A * Bu * J;
-K5_loc_axial = int(K5_loc_axial, -1,1);
-K5_loc_axial_reduced = eval(K5_loc_axial(2,2));
-
-% Moment of inertia as a function of ksi
-I = expand((side^4)/12);
-I = vpa(I,4);
-
-% B matrix for w
-Bw = (2/L5)^2 * [3*ksi/2 (1-3*ksi)*L5/4];
-
-% Numerical integration with 4 gaus points
-
-% Gaus point 1
-w1 = 0.34785; ksi1 = -0.86114;
-Bw5_1 = double(subs(Bw,ksi,ksi1));
-I5_1 = double(subs(I,ksi,ksi1));
-K5_loc_bending_1 = w1 * Bw' * Eb * I * Bw * J;
-K5_loc_bending_1 = double(subs(K5_loc_bending_1,ksi,ksi1));
-
-% Gaus point 2
-w2 = 0.65241; ksi2 = -0.33998;
-Bw5_2 = double(subs(Bw,ksi,ksi2));
-I5_2 = double(subs(I,ksi,ksi2));
-K5_loc_bending_2 = w2 * Bw' * Eb * I * Bw * J;
-K5_loc_bending_2 = double(subs(K5_loc_bending_2,ksi,ksi2));
-
-% Gaus point 3
-w3 = 0.65241; ksi3 = 0.33998;
-Bw5_3 = double(subs(Bw,ksi,ksi3));
-I5_3 = double(subs(I,ksi,ksi3));
-K5_loc_bending_3 = w3 * Bw' * Eb * I * Bw * J;
-K5_loc_bending_3 = double(subs(K5_loc_bending_3,ksi,ksi3));
-
-% Gaus point 4
-w4 = 0.34785; ksi4 = 0.86114;
-Bw5_4 = double(subs(Bw,ksi,ksi4));
-I5_4 = double(subs(I,ksi,ksi4));
-K5_loc_bending_4 = w4 * Bw' * Eb * I * Bw * J;
-K5_loc_bending_4 = double(subs(K5_loc_bending_4,ksi,ksi4));
-
-% Sum the matrices for gaus points
-K5_loc_bending = K5_loc_bending_1 + K5_loc_bending_2 + K5_loc_bending_3 + K5_loc_bending_4;
-
-% Local element matrix 
-K5_loc = [K5_loc_axial_reduced 0 0; 0 K5_loc_bending(1,:); 0 K5_loc_bending(2,:)];
-
-% Transformation matrix
-C15 = cosd(270);
-C25 = cosd(0);
-TM5 = getTransformationMatrix(C15,C25);
-TM5red = applyBCtransformationMatrix(TM5,0,0,0,1,1,1);
-
-% Global element matrix - element 1
-K5red = TM5red' * K5_loc * TM5red;
 
 %% Element 5 - Spring applied at a generic point along the beam
 ksiS = 1/3;
@@ -246,3 +162,47 @@ N = [(1/4) * (1-ksiS)^2*(2+ksiS) ((1-ksiS)^2)*(1+ksiS)*L1/8];
 
 Kspring_5 = N' * ks * N;
 
+%% Assembly stiffness matrix
+K = K2;
+K(1,1) = K(1,1)+K1red(1,1) + Kspring_1(1,1);
+K(1,3) = K(1,3)+K1red(1,3) + Kspring_1(1,2);
+K(2,2) = K(2,2)+K1red(2,2);
+K(3,1) = K(3,1)+K1red(3,1) + Kspring_1(2,1);
+K(3,3) = K(3,3)+K1red(3,3) + Kspring_1(2,2);
+K(4,4) = K(4,4)+K3_global_reduced;
+K_global_reduced = K;
+
+%% Distributed load vector 
+syms ksi
+Nu1 = (1-ksi)/2;
+Nu2 = (1+ksi)/2;
+Nw1 = (1-ksi)^2 * (2+ksi)/4;
+Nfi1 = -(1-ksi)^2 * (1+ksi)*L2/8;
+Nw2 = (1+ksi)^2*(2-ksi)/4;
+Nfi2 = (1+ksi)^2*(1-ksi)*L2/8;
+J = L2/2;
+N = [Nu1 0 0 Nu2 0 0; 0 Nw1 Nfi1 0 Nw2 Nfi2];
+p = [0;p];
+rb = N' * p  * J;
+rb_local = double(int(rb,ksi,-1,1));
+rb_local = [rb_local(1:3); rb_local(5:6)];
+
+Bw = [(1-ksi)^2*(2+ksi)/4 -(1-ksi)^2*(1+ksi)*L2/8 (1+ksi)^2*(2-ksi)/4 (1+ksi)^2*(1-ksi)*L2/8];
+rn_local = Bw' * Q;
+rn_local = double(subs(rn_local,ksi,0));
+rn_local = [0; rn_local(1:2,1); rn_local(3:4,1)];
+r_local = rb_local + rn_local;
+
+r_global = applyBC(TM2,1,1,1,0,1,1)' * r_local;
+
+%% Displacements
+u = inv(K_global_reduced)*r_global;
+
+%% Curvature Kapa
+Kapa = -(2/L2)^2 * [3*ksi/2 L2*(1-3*ksi)/4 -3*ksi/2 -(1+3*ksi)*L2/4];
+ksiM = -1/(2+Z);
+Kapa = double(subs(Kapa,ksi,ksiM));
+Kapa = Kapa*u(2:5,1);
+
+%% Moment at ksiM
+M = EI_24 * Kapa;
